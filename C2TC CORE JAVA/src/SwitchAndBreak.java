0897ac3beb3234a2557bf7aba1ce46470b372e43
Bhumika <bhumikabhoir8894@gmail.com>
2022-08-16 00:43:16 +0530

    Switch and Break

---
+++ C2TC CORE JAVA/src/SwitchAndBreak.java
@@ -0,0 +1,28 @@
+
+public class SwitchAndBreak {
+
+	public static void main(String[] args) {
+		 
+		int a = 0;
+		
+		switch(a) 
+		{
+		   case 1: //my name
+			   System.out.println("My ame is Bhumika Bhoir");
+			   break;  //when condition is get true then break statement stop running the code at that point
+		
+		   case 2: //My age
+			   System.out.println("My age is 22");
+			   break;
+			 
+		   case 3: //about study
+			   System.out.println("I am Engineer");
+			   break;
+		
+			default:  
+				System.out.println("Wrong input");
+		}
+
+	}
+
+}
